793678be56c25f28ac583020b01f1d214dc42012
CHODA V V N KARTHIKEYA MANIKANTA RUTWIK <11249a068@kanchiuniv.ac.in>
2025-11-08 12:44:09 +0530

    Update infix-postfix.c

--- infix-postfix.c
+++ infix-postfix.c
@@ -1,14 +1,44 @@
+                                        //INFIX-POSTFIX//
+
+
+//*Aim : To perform infix to postfix conversion using stack*//
+
+🧭 Algorithm:
+BEGIN
+1. INITIALIZE AN EMPTY STACK AND AN EMPTY POSTFIX STRING.
+2. PUSH '(' ONTO THE STACK AND ADD ')' TO THE END OF THE INFIX EXPRESSION
+3. FOR EACH SYMBOL X IN THE INFIX EXPRESSION, DO:
+a. IF X IS AN OPERAND, ADD X TO THE POSTFIX STRING.
+b. IF X IS '(', PUSH IT ONTO THE STACK.
+c. IF X IS AN OPERATOR, THEN:
+WHILE (STACK IS NOT EMPTY) AND (PRECEDENCE OF TOP OF STACK ≥
+PRECEDENCE OF X):
+POP OPERATOR FROM STACK AND ADD IT TO POSTFIX STRING.
+
+PUSH X ONTO THE STACK.
+d. IF X IS ')', THEN:
+WHILE THE TOP OF THE STACK IS NOT '(':
+POP OPERATOR FROM STACK AND ADD IT TO POSTFIX STRING.
+POP '(' FROM THE STACK AND DISCARD IT.
+
+4. END FOR
+5. THE RESULTING POSTFIX STRING IS THE FINAL OUTPUT.
+END
+
+🧭 Code: 
+    
 #include <stdio.h>
 #include <stdlib.h>
 
 int top = -1, max;
 char *stack;
 
+// Push an element into the stack
 void push(char item)
 {
     if (top == max - 1)
     {
-        printf("stack is overflow\n");
+        printf("Stack is overflow\n");
     }
     else
     {
@@ -17,27 +47,27 @@ void push(char item)
     }
 }
 
+// Pop an element from the stack
 char pop()
 {
-    char item;
     if (top == -1)
     {
-        printf("stack is underflow/empty\n");
+        printf("Stack underflow/empty\n");
         return '\0';
     }
     else
     {
-        item = stack[top];
+        char item = stack[top];
         top = top - 1;
         return item;
     }
 }
 
+// Return the top element without removing it
 char peek()
 {
     if (top == -1)
     {
-        printf("stack is underflow/empty\n");
         return '\0';
     }
     else
@@ -46,6 +76,7 @@ char peek()
     }
 }
 
+// Return precedence value of operators
 int get_precedence(char op)
 {
     switch (op)
@@ -68,22 +99,27 @@ int main()
     printf("Enter the size of infix expression: ");
     scanf("%d", &max);
 
+    // Dynamically allocate memory for stack
     stack = (char *)malloc(max * sizeof(char));
     char infix[max];
     char postfix[max];
 
-    printf("please enter the infix string of size %d: ", max);
+    // Read infix expression from user
+    printf("Please enter the infix string of size %d: ", max);
     scanf("%s", infix);
 
     int token_precedence, i = 0, j = 0;
-    char c, temp;
+    char c;
 
+    // Process each character of infix expression
     while ((c = infix[i]) != '\0')
     {
         token_precedence = get_precedence(c);
+
+        // If operator found, manage precedence
         if (token_precedence > 0)
         {
-            while (get_precedence(peek()) >= token_precedence)
+            while (token_precedence <= get_precedence(peek()))
             {
                 postfix[j++] = pop();
             }
@@ -91,34 +127,39 @@ int main()
         }
         else
         {
+            // Handle parentheses and operands
             switch (c)
             {
             case '(':
                 push(c);
                 break;
             case ')':
-                while (peek() != '(' && peek() != '\0')
+                while (peek() != '(')
                 {
                     postfix[j++] = pop();
                 }
-                pop(); // remove '('
+                pop(); // Remove '(' from stack
                 break;
             default:
-                postfix[j++] = c;
+                postfix[j++] = c; // Operand goes directly to postfix
                 break;
             }
         }
         i++;
     }
 
+    // Pop all remaining operators from stack
     while (peek() != '\0')
     {
         char c = pop();
+        printf("Popped char %c\n", c);
         postfix[j++] = c;
     }
 
+    // End postfix string
     postfix[j] = '\0';
-    printf("postfix notation is : %s\n", postfix);
 
+    // Display final postfix expression
+    printf("Postfix notation is: %s\n", postfix);
     return 0;
 }
