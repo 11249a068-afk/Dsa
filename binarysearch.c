6a3ec009252b576be3495da66f79997214731bc3
CHODA V V N KARTHIKEYA MANIKANTA RUTWIK <11249a068@kanchiuniv.ac.in>
2025-11-08 09:18:20 +0530

    Update binarysearch.c

--- binarysearch.c
+++ binarysearch.c
@@ -1,42 +1,109 @@
+//Aim: To perform aq stack operations using array
+
 #include <stdio.h>
-
-int main() 
+#define MAX 100
+int stack[MAX];
+int top = -1;
+void push(int x)
 {
-    int n, i, key;
-    
-    printf("Enter the size of array: ");
-    scanf("%d", &n);
-    
-    int arr[n];
-    
-    printf("Enter the elements of array in sorted order:\n");
-    for (i = 0; i < n; i++) {
-        printf("Element %d: ", i + 1);
-        scanf("%d", &arr[i]);
+    if (top >= MAX - 1)
+    {
+    printf("Stack overflow\n");
     }
-    
-    printf("Enter the element to search: ");
-    scanf("%d", &key);
-    
-    int start = 0, end = n - 1, mid;
-    
-    while (start <= end) {
-        mid = (start + end) / 2;
-        
-        if (arr[mid] == key) {
-            printf("\nElement found at index %d\n", mid);
-            return 0;
-        }
-        else if (arr[mid] < key) {
-            start = mid + 1;
-        }
-        else {
-            end = mid - 1;
-        }
+    else
+    {
+    top++;
+     stack[top] = x;
     }
-    
-    printf("\nElement not found in the array.\n");
-    return 0;
 }
+int pop()
+{
+    if (top < 0)
+    {
+    printf("Stack underflow\n");
+    return -1;
+    }
+    else
+    {
+    int value = stack[top];
+    top--;
+    return value;
+    }
+}
+int peek()
+{
+    if (top < 0)
+    {
+     printf("Stack is empty\n");
+    return -1;
+    }
+    else
+    {
+    return stack[top];
+    }
+}
+int empty()
+{
+    return top == -1;
+}
+void display()
+{
+    if (empty())
+    {
+     printf("Stack is empty\n");
+    }
+    else
+    {
+    printf("Stack elements: ");
+    for (int i = top; i >= 0; i--)
+    printf("%d ", stack[i]);
+    printf("\n");
+    }
+}
+int main()
+{
+    int choice, value;
+    int n;
+    printf("Enter total size of stack (max %d): ", MAX);
+    scanf("%d", &n);
+    if (n > MAX)
+    {
+     printf("Size exceeds limit\n");
+    return 0;
+    }
+    while (1)
+    {
+    printf("\n1. Push\n2. Pop\n3. Peek\n4. Display\n5. Exit\nEnteryour choice: ");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+             case 1:
+                printf("Enter value to push: ");
+                scanf("%d", &value);
+                push(value);
+                break;
 
+            case 2:
+                value = pop();
+                if (value != -1)
+                    printf("Popped value: %d\n", value);
+                break;
 
+            case 3:
+                value = peek();
+                if (value != -1)
+                    printf("Top value: %d\n", value);
+                break;
+
+            case 4:
+                display();
+                break;
+
+            case 5:
+                return 0;
+
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+}
